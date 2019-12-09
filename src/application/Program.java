81c7aede93ad63cd4b9655ae3d9bd438039626c5
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2019-12-09 20:03:40 -0300

    terceira mensagem 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("primeiro passo");
 		System.out.println("segunda mensagem");
+		System.out.println("terceira mensagem");
 	}
 
 }
